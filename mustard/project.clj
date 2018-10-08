16a66b458b9c809da53af6a6fb36e9fb8ea741f7
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-08 19:26:56 +0300

    Add base project file

---
+++ mustard/project.clj
@@ -0,0 +1,20 @@
+(defproject mustard "0.1.0"
+  :description "Yet another Clojure static code analyze tool."
+  :url "https://github.com/stormaaja/mustard"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :plugins [[lein-cljfmt "0.6.0"]
+            [lein-kibit "0.1.6"]
+            [lein-bikeshed "0.5.1"]
+            [jonase/eastwood "0.3.1"]
+            [lein-auto "0.1.3"]
+            [lein-ancient "0.6.15"]
+            [lein-cloverage "1.0.13"]]
+  :cloverage {:html? false}
+  :cljfmt {:indents {#".*" [[:block 0]]}}
+  :aliases {"checkall" ["do"
+                        ["kibit"]
+                        ["bikeshed"]
+                        ["eastwood"]
+                        ["cljfmt" "check"]]})
