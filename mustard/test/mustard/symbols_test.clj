7c66f3767a4305d783e0dda9726fec5b576f2e4d
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-16 20:56:12 +0300

    Add symbols tests

--- mustard/test/mustard/symbols_test.clj
+++ mustard/test/mustard/symbols_test.clj
@@ -2,12 +2,24 @@
   (:require [clojure.test :refer :all]
             [mustard.symbols :as s]))
 
-(deftest get-symbols
+(deftest get-symbols-test
   (testing "Get symbols"
     (is (= (set (s/get-symbols
                   '((ns mustard.test
                       (:require [hello.world :as h]))
-                    (defn hello [w]
-                      (prn (h/p w))))))
+                     (defn hello [w]
+                       (prn (h/p w))))))
            (set '(ns mustard.test :require hello.world :as
                      h defn hello w prn h/p w))))))
+
+(deftest get-used-namespaces-test
+  (testing "Get used namespaces"
+    (is (= (s/get-used-namespaces '()) #{}))
+    (is (= (s/get-used-namespaces '(defn hello h/p w prn ot/calculate))
+           '#{h ot}))))
+
+(deftest get-namespace-symbol-test
+  (testing "Get namespace symbol"
+    (is (= (s/get-namespace-symbol nil) nil))
+    (is (= (s/get-namespace-symbol 'hello) nil))
+    (is (= (s/get-namespace-symbol 'hello/world) 'hello))))
