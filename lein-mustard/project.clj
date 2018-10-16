5843bf74f1a87fa47b40b43224cee7de1973ec67
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-16 20:10:19 +0300

    Add leiningen plugin project file

---
+++ lein-mustard/project.clj
@@ -0,0 +1,6 @@
+(defproject lein-mustard "0.1.0"
+  :description "Leiningen plugin for Mustard"
+  :url "https://github.com/stormaaja/mustard"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
