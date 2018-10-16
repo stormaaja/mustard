ae71377d0b744fd845bec4cdfe95ba7a16dd5ed1
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-16 19:06:11 +0300

    Add function for finding clojure files

--- mustard/src/mustard/file.clj
+++ mustard/src/mustard/file.clj
@@ -1,4 +1,13 @@
-(ns mustard.file)
+(ns mustard.file
+  (:require [clojure.java.io :as io]))
 
 (defn read-file-symbols [path]
   (read-string (str "(" (slurp path) ")")))
+
+(defn clj-file? [file]
+  (some? (re-find #".*\.cljs?$" (.getName file))))
+
+(defn find-clj-files [folder]
+  (filter
+   clj-file?
+   (file-seq (io/file folder))))
