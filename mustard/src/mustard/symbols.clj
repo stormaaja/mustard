49823b4c40af01b3e2319512c1893e97d4afa786
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-21 09:14:46 +0300

    Add validating code with simple evaluator

--- mustard/src/mustard/symbols.clj
+++ mustard/src/mustard/symbols.clj
@@ -1,5 +1,14 @@
 (ns mustard.symbols)
 
+(defn- evaluator [x] x)
+
+(defn valid-code? [v]
+  (some?
+    (try
+      (evaluator v)
+      (catch Exception e
+        nil))))
+
 (defn get-symbols [code]
   (reduce
     (fn [c n]
@@ -10,7 +19,7 @@
     code))
 
 (defn get-namespace-symbol [s]
-  (when (and (symbol? s) (namespace s))
+  (when (and (valid-code? s) (symbol? s) (namespace s))
     (symbol (namespace s))))
 
 (defn get-used-namespaces [symbols]
