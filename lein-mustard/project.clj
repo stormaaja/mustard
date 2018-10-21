12014c80027a7dfd5cabb1d1841e3b3f36157802
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-21 11:34:09 +0300

    Update project version

--- lein-mustard/project.clj
+++ lein-mustard/project.clj
@@ -1,7 +1,7 @@
-(defproject lein-mustard "0.1.2"
+(defproject lein-mustard "0.1.3"
   :description "Leiningen plugin for Mustard"
   :url "https://github.com/stormaaja/mustard"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :dependencies [[mustard "0.1.2"]])
+  :dependencies [[mustard "0.1.3"]])
