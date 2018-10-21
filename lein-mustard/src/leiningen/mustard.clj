653f75cf98c4109303ceff74ccd759e611fb1217
Matti Ahinko <matti.ahinko@gmail.com>
2018-10-21 09:23:40 +0300

    Add using straight mustard project

--- lein-mustard/src/leiningen/mustard.clj
+++ lein-mustard/src/leiningen/mustard.clj
@@ -1,27 +1,12 @@
 (ns leiningen.mustard
   (:require [leiningen.core.eval :as eval]
-            [leiningen.core.main :as main])
+            [leiningen.core.main :as main]
+            [mustard.core :as m])
   (:import (clojure.lang ExceptionInfo)))
 
-(defn get-mustard-dependency [project]
-  (some
-    #(when (= (first %) 'mustard) %)
-    (:dependencies project)))
-
-(defn has-mustard? [project]
-  (some? (get-mustard-dependency project)))
-
-(defn mustard [project & args]
-  (let [project (if (has-mustard? project)
-                  project
-                  (update-in project [:dependencies]
-                             conj ['mustard "RELEASE"]))
-        opts project]
-    (try
-      (eval/eval-in-project
-        project
-        `(mustard.core/run-project '~opts ~@args)
-        '(require 'mustard.core))
-      (catch ExceptionInfo e
-        (main/exit
-          (:exit-code (ex-data e) 1))))))
+(defn ^:no-project-needed mustard [project & args]
+  (try
+    (m/run-project project args)
+    (catch ExceptionInfo e
+      (main/exit
+        (:exit-code (ex-data e) 1)))))
